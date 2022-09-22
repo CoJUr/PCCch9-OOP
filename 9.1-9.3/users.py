1f48473e3d758e34e6125da9ba768f7b952758fe
“Cory <coryurton@gmail.com>
2022-09-21 20:35:10 -0500

    cleaning and verifying functionality of 9.1-9.3

--- 9.1-9.3/users.py
+++ 9.1-9.3/users.py
@@ -1,5 +1,6 @@
 class User:
     """A simple model of a User class"""
+
     def __init__(self, first_name, last_name, username, email, location):
         """Initialize the attributes"""
         self.first_name = first_name
@@ -8,15 +9,28 @@ class User:
         self.email = email
         self.location = location
 
-        def describe_user(self):
-            """Prints a summary of the user's information"""
-            print(f"{self.first_name} {self.last_name} is from {self.location}."
-                  f"\n{self.first_name} can be reached at {self.email}."
-                  f"\n{self.first_name}'s username is {self.username}.")
+    def describe_user(self):
+        """Prints a summary of the user's information"""
+        print(f"{self.first_name} {self.last_name} is from {self.location}."
+              f"\n{self.first_name} can be reached at {self.email}."
+              f"\n{self.first_name}'s username is {self.username}.")
 
-            def greet_user(self):
-                """Prints a personalized greeting to the user"""
-                print(f"Hello {self.first_name} {self.last_name}!")
+    def greet_user(self):
+        """Prints a personalized greeting to the user"""
+        print(f"Hello {self.first_name} {self.last_name}!")
 
-                user_1 = User('joe', 'smith', 'jsmith', 'jsmith@email.com',
-                              'New York, NY')
+
+user_1 = User('joe', 'smith', 'jsmith', 'jsmith@email.com',
+              'New York, NY')
+
+user_2 = User('jane', 'doe', 'jdoe', 'jdoe@email.com',
+              'Los Angeles, CA')
+
+user_3 = User('jim', 'smith', 'jsmith', 'jsmith@email.com',
+              'Chicago, IL')
+
+user_1.describe_user()
+user_1.greet_user()
+print("#######")
+user_2.describe_user()
+user_2.greet_user()
