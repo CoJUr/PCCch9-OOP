8ad85e119e887f2ff70964b216bc0b89933bf192
“Cory <coryurton@gmail.com>
2022-09-27 17:26:52 -0500

    no idea, syncing up

--- edabit/basic_calc.py
+++ edabit/basic_calc.py
@@ -17,8 +17,63 @@ def calculator(num1, operator, num2):
         except ZeroDivisionError:
             return "Can't divide by 0!"
 
+
 print(calculator(10, "+", 5))  # 15
 print(calculator(10, "-", 5))  # 5
 print(calculator(10, "*", 5))  # 50
 print(calculator(10, "/", 5))  # 2.0
 
+
+# others:
+
+def calculator(num1, operator, num2):
+    if operator == '/' and num2 == 0:
+        return "Can't divide by 0!"
+    return eval('num1' + operator + 'num2')
+
+
+def calculator(num1, operator, num2):
+    if operator == "/" and num2 == 0:
+        return "Can't divide by 0!"
+    if operator == "+":
+        return num1 + num2
+    if operator == "-":
+        return num1 - num2
+    if operator == "*":
+        return num1 * num2
+    if operator == "/":
+        return num1 / num2
+
+
+def calculator(num1, operator, num2):
+    return "Can't divide by 0!" if num2 == 0 and operator == '/' else eval(str(num1) + operator + str(num2))
+
+
+def calculator(num1, operator, num2):
+    if operator == "/":
+        if num2 == 0:
+            return "Can't divide by 0!"
+        else:
+            return num1 / num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    else:
+        return "What?"
+
+
+def calculator(num1, operator, num2):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        try:
+            return num1 / num2
+        except ZeroDivisionError:
+            return "Can't divide by 0!"
