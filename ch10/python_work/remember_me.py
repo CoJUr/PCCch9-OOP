a2ba25d4eb81086c7923cf52cf379d31d775db00
“Cory <coryurton@gmail.com>
2022-09-27 22:35:02 -0500

    sleepy

--- ch10/python_work/remember_me.py
+++ ch10/python_work/remember_me.py
@@ -11,16 +11,43 @@ try-except block.
 
 import json
 
-# load username if its stored already, else prompt for it and store it
-filename = 'json_files/username.json'
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
-    username = input("What is your name? ")
-    with open(filename, 'w') as f:
-        json.dump(username, f)
-        print(f"We'll remember you when you come back, {username}!")
-else:
-    print(f"Welcome back, {username}!")
 
+def greet_user():
+    """Refactoring. Greet the user by name (do-one-thing rule)."""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = input("What is your name? ")
+        filename = 'json_files/username.json'
+        with open(filename, 'w') as f:
+            json.dump(username, f)
+            print(f"We'll remember you when you come back, {username}!")
+
+
+# filename = 'json_files/username.json'
+# try:
+#     with open(filename) as f:
+#         username = json.load(f)
+# except FileNotFoundError:
+#     username = input("What is your name? ")
+#     with open(filename, 'w') as f:
+#         json.dump(username, f)
+#         print(f"We'll remember you when you come back, {username}!")
+# else:
+#     print(f"Welcome back, {username}!")
+
+
+def get_stored_username():
+    """Get stored username if available."""
+    filename = 'json_files/username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+
+greet_user()
